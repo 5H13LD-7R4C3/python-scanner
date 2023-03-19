645a81d743a4ca6385c77f91d5efd2cc22936719
Player-One <63012347+5H13LD-7R4C3@users.noreply.github.com>
2023-03-19 01:41:17 +0000

    .

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ import nmap
 import sqlite3
 import os
 from forms import RegistrationForm, ScanForm
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
 from models import User, Scan
 from config import Config
 
@@ -44,24 +41,6 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user is None or not check_password_hash(user.password_hash, form.password.data):
-            flash('Invalid email or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route('/logout')
