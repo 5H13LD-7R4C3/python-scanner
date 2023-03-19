cff22d9322ea916fbadb7fa05ea93add4e38b7b6
Player-One <63012347+5H13LD-7R4C3@users.noreply.github.com>
2023-03-19 01:33:42 +0000

    .

--- app/models.py
+++ app/models.py
@@ -1,26 +1,29 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
 from datetime import datetime
-
-db = SQLAlchemy()
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app.database import db
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-    scans = db.relationship('Scan', backref='user', lazy=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    scans = db.relationship('Scan', backref='owner', lazy='dynamic')
 
     def __repr__(self):
-        return f"User('{self.username}')"
+        return '<User {}>'.format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Scan(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(500), nullable=False)
-    ip_addresses = db.Column(db.String(500), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    url = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return f"Scan('{self.name}', '{self.description}', '{self.ip_addresses}', '{self.date_created}')"
+        return '<Scan {}>'.format(self.url)
