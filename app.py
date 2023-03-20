fad7e0020c3c91fd18a410c5a06beb38bc309763
Player-One <63012347+5H13LD-7R4C3@users.noreply.github.com>
2023-03-20 20:44:49 +0000

    .

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from werkzeug.urls import url_parse
 from werkzeug.security import generate_password_hash, check_password_hash
 from config import Config
 from datetime import datetime
-import database
+
 
 from flask_login import UserMixin
 from flask_wtf import FlaskForm
@@ -22,6 +22,74 @@ app.config.from_object(Config)
 login = LoginManager(app)
 login.login_view = 'login'
 
+class Database:
+    def __init__(self, db_name):
+        self.db_name = db_name
+
+    def create_tables(self):
+        conn = sqlite3.connect(self.db_name)
+        c = conn.cursor()
+
+        c.execute('''CREATE TABLE IF NOT EXISTS scans
+            (id INTEGER PRIMARY KEY AUTOINCREMENT,
+             ip_address TEXT,
+             port_number TEXT,
+             report TEXT,
+             scan_time TEXT,
+             user_id INTEGER,
+             FOREIGN KEY (user_id) REFERENCES users(id))''')
+
+        c.execute('''CREATE TABLE IF NOT EXISTS users
+            (id INTEGER PRIMARY KEY AUTOINCREMENT,
+             username TEXT UNIQUE,
+             password TEXT)''')
+
+        conn.commit()
+        conn.close()
+
+    def add_user(self, username, password):
+        conn = sqlite3.connect(self.db_name)
+        c = conn.cursor()
+
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        c.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, hashed_password))
+
+        conn.commit()
+        conn.close()
+
+    def get_user(self, username):
+        conn = sqlite3.connect(self.db_name)
+        c = conn.cursor()
+
+        c.execute('SELECT * FROM users WHERE username=?', (username,))
+        user = c.fetchone()
+
+        conn.commit()
+        conn.close()
+
+        return user
+
+    def add_scan(self, ip_address, port_number, report, scan_time, user_id):
+        conn = sqlite3.connect(self.db_name)
+        c = conn.cursor()
+
+        c.execute('INSERT INTO scans (ip_address, port_number, report, scan_time, user_id) VALUES (?, ?, ?, ?, ?)',
+                  (ip_address, port_number, report, scan_time, user_id))
+
+        conn.commit()
+        conn.close()
+
+    def get_scans_by_user(self, user_id):
+        conn = sqlite3.connect(self.db_name)
+        c = conn.cursor()
+
+        c.execute('SELECT * FROM scans WHERE user_id=?', (user_id,))
+        scans = c.fetchall()
+
+        conn.commit()
+        conn.close()
+
+        return scans
 
 @login.user_loader
 def load_user(id):
