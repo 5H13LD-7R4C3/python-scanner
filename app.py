3368ba3a270aaeefd54da7719c58be2502d76cf6
Player-One <63012347+5H13LD-7R4C3@users.noreply.github.com>
2023-03-20 20:37:16 +0000

    ,

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from flask_login import LoginManager, login_user, logout_user, current_user, log
 from werkzeug.urls import url_parse
 from werkzeug.security import generate_password_hash, check_password_hash
 from config import Config
-from models import User, Scan
+from models import Scan
 from formss import RegistrationForms, ScanForm
-
+from models import User
 # Initialize Flask app
 app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'templates'), static_folder=os.path.join(os.path.dirname(__file__), 'static'))
 
