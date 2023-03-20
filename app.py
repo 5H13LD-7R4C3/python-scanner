40eda65aef4c000a7e1537025a4f90ef89c537e6
Player-One <63012347+5H13LD-7R4C3@users.noreply.github.com>
2023-03-20 20:24:33 +0000

    .

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import nmap
 import sqlite3
 import os
-from ..form import RegistrationForm, ScanForm
+from formss import RegistrationForm, ScanForm
 from models import User, Scan
 from config import Config
 
