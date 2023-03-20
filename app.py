c6b1aff0055cd4f3e3f577b3887cbc7494c64063
Player-One <63012347+5H13LD-7R4C3@users.noreply.github.com>
2023-03-20 20:43:22 +0000

    .

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from werkzeug.urls import url_parse
 from werkzeug.security import generate_password_hash, check_password_hash
 from config import Config
 from datetime import datetime
+import database
 
 from flask_login import UserMixin
 from flask_wtf import FlaskForm
